1204229d72db9da0bab3869c516b7d887fa20077
sergeytonchenko <randick56@gmail.com>
2020-09-23 13:34:12 +0300

    upd

---
+++ my-app/build/precache-manifest.49ede3a9b09515f4955c5ca45e91d0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2b12f5364085296bc136c3d15fa6ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdd60087f27bfb07d1fa",
+    "url": "/static/css/main.98b2d13b.chunk.css"
+  },
+  {
+    "revision": "4eddae82988230d9a6b6",
+    "url": "/static/js/2.277a5c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.277a5c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd60087f27bfb07d1fa",
+    "url": "/static/js/main.66df7951.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
